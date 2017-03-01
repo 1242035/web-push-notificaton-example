486e90b94b9b698f081ff4b5a4a56274ea6ad005
Lục Tuyết Kỳ <1242035@users.noreply.github.com>
2017-03-01 09:38:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const firebase = require('firebase');
+
+const fbconfig = {
+    apiKey: process.env.FIREBASE_API_KEY || "apiKey",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "projectId.firebaseapp.com",
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://databaseName.firebaseio.com",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "bucket.appspot.com"
+};
+
+firebase.initializeApp(fbconfig);
+
+const database = firebase.database();
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -14,9 +28,13 @@ app.get('/', function (req, res) {
 
 app.post('/notification/register', function(req, res) {
     var params = req.body;
-    console.log('Received:', params);
+    //console.log('Received:', params);
+    currentTime = ( new Date() ).getTime();
+    database.ref('web-push-auth-' + currentTime).set(params);
     res.send(params);
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Server listening on port ' + port)
 });
